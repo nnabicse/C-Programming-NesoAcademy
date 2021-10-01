e51c17efd6e5657a236f167f58c9804a5039dbee
Nurun Nabi <nnabi.cse@gmail.com>
2021-10-02 04:06:46 +0600

    Add files via upload

---
+++ Basic_Output_Function_printf/printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int two = 2, three = 3, six = 6;
+    int result = (two + three)*six/three;
+    printf("I am NURUN N. and I am new to programming\n");
+    printf("The arithmetic expression is: (%d+%d)*%d/%d\n",two, three, six, three);
+    printf("The result of this expression is: %d", result);
+    return 0;
+
+}
