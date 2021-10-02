e6b6e15693bb843561b0adb06d35815d039e0efc
Nurun Nabi <nnabi.cse@gmail.com>
2021-10-03 02:01:11 +0600

    Add files via upload

---
+++ Fundamental_Data_Types_Character/char_signed_unsigned.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    char var1 = 128;
+    char var2 = -128;
+
+    char var3 = 129;
+    char var4 = -127;
+
+    printf("%c\n",var1);
+    printf("%c\n",var2);
+
+    printf("%c\n",var3);
+    printf("%c\n",var4);
+
+
+    return 0;
+}
+
---
+++ Fundamental_Data_Types_Character/print_char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    char var1 = 'A';
+    char var2 = 65;
+
+    printf("%c\n",var1);
+    printf("%c\n",var2);
+
+    return 0;
+}
