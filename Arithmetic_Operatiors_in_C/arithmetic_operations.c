84f84fd2ffc041cf2cf8c51be9bfd07048a73172
Nurun Nabi <nnabi.cse@gmail.com>
2021-10-05 02:14:34 +0600

    Add files via upload

---
+++ Arithmetic_Operatiors_in_C/arithmetic_operations.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter First Number: ");
+    scanf("%d",&a);
+    printf("Enter Second Number: ");
+    scanf("%d",&b);
+
+    printf("a+b = %d\n",a+b);
+    printf("a-b = %d\n",a-b);
+    printf("a*b = %d\n",a*b);
+    printf("a/b = %d\n",a/b);
+    printf("a%b = %d\n",a%b);
+
+    return 0;
+
+}
